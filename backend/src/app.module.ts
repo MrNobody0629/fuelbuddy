fd03a8f1e535243fa7c225487d227a2dfa7ec373
Anuj verma <anujv962@gmail.com>
2025-05-04 15:02:53 +0530

    Backend server Boiler plate added which includes basic packages installation

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+}
