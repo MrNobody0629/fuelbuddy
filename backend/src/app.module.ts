5011d49bcadebef9625e4bd4336ba581b2314dc0
Anuj verma <anujv962@gmail.com>
2025-05-04 15:08:49 +0530

    Db Connection created using postgresql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,6 +7,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      logging: true,
+      autoLoadEntities: true,
+      synchronize: process.env?.ENV === 'dev',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
